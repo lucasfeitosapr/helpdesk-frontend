930e7334230acff995a1e79e30ca770accd9605f
Lucas Feitosa <lucasfeitosapr@gmail.com>
2023-06-28 12:42:31 -0300

    Criação do componente chamado-create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 
 @NgModule({
@@ -56,7 +57,8 @@ import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-
     ClienteUpdateComponent,
     ClienteDeleteComponent,
     ClienteListComponent,
-    ChamadoListComponent
+    ChamadoListComponent,
+    ChamadoCreateComponent
     
   ],
   imports: [
