9c723217fb748aa0f6cc7d3e2cf931ba1185c0bc
Lucas Feitosa <lucasfeitosapr@gmail.com>
2023-06-28 13:02:29 -0300

    Criação do componente de update no chamado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
@@ -29,7 +30,8 @@ const routes: Routes = [
       { path: 'clientes/update/:id', component: ClienteUpdateComponent},
       { path: 'clientes/delete/:id', component: ClienteDeleteComponent},
 
-      { path: 'chamados', component: ChamadoListComponent }
+      { path: 'chamados', component: ChamadoListComponent },
+      { path: 'chamados/create', component: ChamadoCreateComponent }
 
 
 
